99fe9259a5bf10b84f696c6801b92d2bb6ff1edb
aliamatyanniversary <mrkusmatej25@gmail.com>
2026-06-20 22:48:49 +0200

    Update mail.js

--- js/mail.js
+++ js/mail.js
@@ -50,7 +50,7 @@ const mails = [
   },
     {
     from: 'Matýsek',
-    subject: 'Love letter',
+    subject: 'Last school letter',
     snippet: 'Náš poslední den...',
     password: 'wednesdat',
     body: `Náš poslední den spolu bude nejhezčí a nejkrásnější, jsem si tím dost jistý. To léto nějak dáme, máme toho hoodně připraveného a já mám pro tebe dva dárky naplánové :) Snad se budou líbit! Missuju tě už teďka. Love you.`,
